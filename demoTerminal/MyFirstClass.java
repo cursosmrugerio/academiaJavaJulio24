c50c163d6847424b7890de116c5c3f3c75de832a
Miguel Rugerio <mrugerio@gmail.com>
2024-07-17 09:53:38 -0600

    Linea de comando & args[]

---
+++ demoTerminal/MyFirstClass.java
@@ -0,0 +1,7 @@
+public class MyFirstClass {
+
+	public static void main(String... patitos) {
+		System.out.println("Segundo Argumento: "+patitos[1]);
+	}
+
+}
---
+++ simuladorBasic/src/simuladorBasic/MyFirstClass.java
@@ -0,0 +1,9 @@
+package simuladorBasic;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println(args[1]);
+	}
+
+}
