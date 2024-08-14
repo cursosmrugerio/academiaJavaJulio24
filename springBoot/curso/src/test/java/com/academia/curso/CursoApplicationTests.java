27da497bcb0f08b4426ffffec7cdd1e7950f731f
Miguel Rugerio <mrugerio@gmail.com>
2024-08-14 17:34:05 -0600

    Rest con SpringBoot

---
+++ springBoot/curso/src/test/java/com/academia/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
